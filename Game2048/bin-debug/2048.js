3d9c05297666c813a40c9ee53674a249b07427e7
sunyijie1314 <1052112715@qq.com>
2020-01-01 23:04:58 +0800

    16 has some bug, I can't think Promise is so strong, so I can finish it easliy!

--- Game2048/bin-debug/2048.js
+++ Game2048/bin-debug/2048.js
@@ -18,12 +18,6 @@ var Game = (function (_super) {
         _this.m_isMerge = false;
         _this.m_isOver = false;
         _this.m_isMoved = false;
-        _this.m_aniCount = 0;
-        _this.m_aniDataArr = [];
-        // for (var i = 0; i < Main.m_sNumX * Main.m_sNumY; i++ )
-        // {
-        //     this.m_isGrid[i] = false;
-        // }
         _this.addEventListener(egret.Event.ADDED_TO_STAGE, _this.onAddToStage, _this);
         return _this;
     }
@@ -34,7 +28,6 @@ var Game = (function (_super) {
     //开始
     Game.prototype.start = function () {
         for (var i = 0; i < Main.m_sNumX * Main.m_sNumY; i++) {
-            // this.m_isGrid[i] = false;
             if ((undefined !== this.m_grids[i]) && (undefined !== this.m_grids[i].parent)) {
                 this.m_grids[i].parent.removeChild(this.m_grids[i]);
                 this.m_grids[i] = undefined;
@@ -143,13 +136,9 @@ var Game = (function (_super) {
         var toX = (vRow + 1) * Main.m_sSpace + vRow * Main.m_sGridWidth;
         var toY = (vCol + 1) * Main.m_sSpace + vCol * Main.m_sGridHeight;
         var data = new Data(next, start, toX - fromX, toY - fromY, 50 * time, isMerge);
-        this.m_aniDataArr.push(data);
     };
     Game.prototype.merge = function (startIndexs, nextNum, layerNum) {
-        if (0 != this.m_aniDataArr.length) {
-            console.log("aniDataArr is not clear, lenth is " + this.m_aniDataArr.length);
-            this.m_aniDataArr.length = 0;
-        }
+        var _this = this;
         if (true == this.m_isMerge) {
             console.log("now is merging.");
             return;
@@ -160,84 +149,50 @@ var Game = (function (_super) {
         }
         console.log("----------------------start----------------------");
         this.m_isMerge = true;
+        var mergePromises = [];
         for (var _i = 0, startIndexs_1 = startIndexs; _i < startIndexs_1.length; _i++) {
             var startIndex = startIndexs_1[_i];
             var next = startIndex + nextNum;
             var end = startIndex + nextNum * (layerNum - 1);
             for (var start = startIndex; start !== end;) {
-                if (0 != this.m_aniCount) {
-                    continue;
-                }
-                // while (false == this.m_isGrid[next])
                 while (undefined == this.m_grids[next]) {
-                    // if ((next == end) && (false == this.m_isGrid[end]))
                     if ((next == end) && (undefined == this.m_grids[end])) {
                         break;
                     }
                     next = next + nextNum;
                 }
-                // if (false == this.m_isGrid[next])
                 if (undefined == this.m_grids[next]) {
                     start = start + nextNum;
                     next = start + nextNum;
                     continue;
                 }
                 var lSpace = Math.abs((start - next) / nextNum);
+                var vRow = start % Main.m_sNumX;
+                var vCol = Math.floor(start / Main.m_sNumY);
+                var fromX = this.m_grids[next].getX();
+                var fromY = this.m_grids[next].getY();
+                var toX = (vRow + 1) * Main.m_sSpace + vRow * Main.m_sGridWidth;
+                var toY = (vCol + 1) * Main.m_sSpace + vCol * Main.m_sGridHeight;
                 //移动到空位置
-                // if (false == this.m_isGrid[start])
                 if (undefined == this.m_grids[start]) {
-                    this.m_grids[start] = new Grid();
-                    this.m_grids[start].copy(this.m_grids[next]);
-                    this.setPosition(start, this.m_grids[start]);
-                    this.addChild(this.m_grids[start]);
-                    this.m_grids[start].visible = false;
-                    // this.m_isGrid[start] = true;
-                    // this.m_isGrid[next] = false;
-                    this.animateMove(start, next, lSpace, false);
-                    // while(0 < this.m_aniDataArr.length)
-                    // {
-                    //     this.moveAni(this.m_aniDataArr[0], false);
-                    //     this.m_aniDataArr.shift();
-                    // }
-                    // if (undefined !== this.m_grids[next].parent)
-                    // {
-                    //     this.m_grids[next].parent.addChild(this.m_grids[start]);
-                    //     // this.m_grids[next].parent.removeChild(this.m_grids[next]);
-                    //     this.m_grids[next] = undefined;
-                    // this.m_isGrid[start] = true;
-                    // this.m_isGrid[next] = false;
-                    // }
-                    // this.m_isMoved = true;   
+                    this.m_grids[start] = this.m_grids[next];
+                    mergePromises.push(this.moveBy(toX - fromX, toY - fromY, 50 * lSpace, this.m_grids[next]));
+                    this.m_grids[next] = undefined;
                 }
                 else if (this.m_grids[start].getText() == this.m_grids[next].getText()) {
-                    // var vGrid:Grid = new Grid();
-                    // let num = Number(this.m_grids[start].getText());
-                    // let txt = String(num * 2);
-                    // let numInfo = Util.getNumInfo(num * 2);
-                    // this.m_grids[start].setText(txt);
-                    // this.m_grids[start].setColor(numInfo.backgroundColor);
-                    // this.animateMove(start, next, lSpace, true);
-                    // while(0 < this.m_aniDataArr.length)
-                    // {
-                    //     this.mergeAni(this.m_aniDataArr[0], false);
-                    //     this.m_aniDataArr.shift();
-                    // }
-                    // vGrid.copy(this.m_grids[start]);
-                    // if ((undefined !== this.m_grids[next].parent) && (undefined !== this.m_grids[start].parent))
-                    // {
-                    //     //巨坑 不重新添加无法更新自己状态
-                    //     // this.m_grids[start].parent.removeChild(this.m_grids[start]);
-                    //     // this.m_grids[start] = vGrid;
-                    //     this.m_grids[next].parent.addChild(this.m_grids[start]);
-                    //     this.m_grids[next].parent.removeChild(this.m_grids[next]);
-                    //     this.m_grids[next] = undefined;
-                    // }
-                    // this.m_isGrid[next] = false;
+                    var num = Number(this.m_grids[start].getText());
+                    var txt = String(num * 2);
+                    var numInfo = Util.getNumInfo(num * 2);
+                    this.m_grids[start].setText(txt);
+                    this.m_grids[start].setColor(numInfo.backgroundColor);
+                    this.removeChild(this.m_grids[start]);
+                    this.addChild(this.m_grids[start]);
+                    mergePromises.push(this.moveByAndFadeOut(toX - fromX, toY - fromY, 50 * lSpace, this.m_grids[next]));
+                    this.m_grids[next] = undefined;
                     start = start + nextNum;
                     next = start + nextNum;
                     continue;
                 }
-                // if ((end == next) || ((next == start + nextNum) && (true == this.m_isGrid[next])))
                 if ((end == next) || ((next == start + nextNum) && (undefined != this.m_grids[next]))) {
                     start = start + nextNum;
                     next = start + nextNum;
@@ -248,89 +203,46 @@ var Game = (function (_super) {
                 }
             }
         }
-        if (undefined == this.m_aniDataArr) {
-            this.m_isMerge = false;
+        if (mergePromises.length > 0) {
+            // 待所有动画结束后， 将 this.m_isMerge 置为 false
+            Promise.all(mergePromises).then(function () {
+                _this.m_isMerge = false;
+                _this.randomGrid();
+                if (true == _this.isWin()) {
+                    console.log("YOU GOT IT!");
+                }
+            });
         }
         else {
-            this.m_aniCount = this.m_aniDataArr.length;
-        }
-        this.animate();
-    };
-    Game.prototype.animate = function () {
-        while ((undefined != this.m_aniDataArr) && (0 < this.m_aniDataArr.length)) {
-            console.log("length:" + length);
-            for (var i = 0; i < this.m_aniDataArr.length; i++) {
-                console.log("m_orgGirdNum:" + this.m_aniDataArr[i].m_orgGirdNum);
-                console.log("m_nowGirdNum:" + this.m_aniDataArr[i].m_nowGirdNum);
-                console.log("m_x:" + this.m_aniDataArr[i].m_x);
-                console.log("m_y:" + this.m_aniDataArr[i].m_y);
-                console.log("m_time:" + this.m_aniDataArr[i].m_time);
-                console.log("m_isMerge:" + this.m_aniDataArr[i].m_isMerge);
-                console.log("-------------------------------------------------------");
-            }
-            var dataTemp = this.m_aniDataArr[0];
-            if (true == dataTemp.m_isMerge) {
-                this.mergeAni(dataTemp, true);
-            }
-            else {
-                this.moveAni(dataTemp, true);
-            }
-            this.m_aniDataArr.shift();
-        }
-    };
-    Game.prototype.mergeAni = function (dataTemp, out) {
-        var _this = this;
-        egret.Tween.get(this.m_grids[dataTemp.m_orgGirdNum])
-            .to({ x: dataTemp.m_x, y: dataTemp.m_y }, dataTemp.m_time, egret.Ease.sineIn)
-            .call(function () {
-            if (undefined !== _this.m_grids[dataTemp.m_orgGirdNum]) {
-                var num = Number(_this.m_grids[dataTemp.m_nowGirdNum].getText());
-                var txt = String(num * 2);
-                var numInfo = Util.getNumInfo(num * 2);
-                _this.m_grids[dataTemp.m_nowGirdNum].setText(txt);
-                _this.m_grids[dataTemp.m_nowGirdNum].setColor(numInfo.backgroundColor);
-                _this.m_grids[dataTemp.m_orgGirdNum].parent.removeChild(_this.m_grids[dataTemp.m_nowGirdNum]);
-                _this.m_grids[dataTemp.m_orgGirdNum].parent.addChild(_this.m_grids[dataTemp.m_nowGirdNum]);
-                _this.m_grids[dataTemp.m_orgGirdNum].parent.removeChild(_this.m_grids[dataTemp.m_orgGirdNum]);
-                _this.m_grids[dataTemp.m_orgGirdNum] = undefined;
-            }
-            // this.m_isGrid[dataTemp.m_orgGirdNum] = false;
-            _this.m_isMoved = true;
-            if (true == out) {
-                _this.judge();
-            }
-        });
-    };
-    Game.prototype.moveAni = function (dataTemp, out) {
-        var _this = this;
-        egret.Tween.get(this.m_grids[dataTemp.m_orgGirdNum])
-            .to({ x: dataTemp.m_x, y: dataTemp.m_y }, dataTemp.m_time, egret.Ease.sineIn)
-            .call(function () {
-            _this.m_grids[dataTemp.m_nowGirdNum].visible = true;
-            if (undefined !== _this.m_grids[dataTemp.m_orgGirdNum]) {
-                _this.m_grids[dataTemp.m_orgGirdNum].parent.removeChild(_this.m_grids[dataTemp.m_orgGirdNum]);
-                _this.m_grids[dataTemp.m_orgGirdNum] = undefined;
-                // this.m_isGrid[dataTemp.m_nowGirdNum] = true;
-                // this.m_isGrid[dataTemp.m_orgGirdNum] = false;
-            }
-            _this.m_isMoved = true;
-            if (true == out) {
-                _this.judge();
-            }
-        });
-    };
-    Game.prototype.judge = function () {
-        this.m_aniCount--;
-        if (0 != this.m_aniCount) {
-            return;
-        }
-        if (true == this.m_isMoved && 0 == this.m_aniDataArr.length) {
             this.m_isMerge = false;
-            this.randomGrid();
-            if (true == this.isWin()) {
-                console.log("YOU GOT IT!");
-            }
         }
+        //this.animate();
+    };
+    Game.prototype.moveBy = function (x, y, time, grid) {
+        return new Promise(function (resolve) {
+            egret.Tween.get(grid)
+                .to({
+                x: x,
+                y: y
+            }, time, egret.Ease.sineInOut)
+                .call(resolve);
+        });
+    };
+    Game.prototype.moveByAndFadeOut = function (x, y, time, grid) {
+        return new Promise(function (resolve) {
+            egret.Tween.get(grid)
+                .to({
+                x: x,
+                y: y
+            }, time, egret.Ease.sineInOut)
+                .to({
+                alpha: 0
+            }, 250)
+                .call(function () {
+                grid.parent.removeChild(grid);
+                resolve();
+            });
+        });
     };
     Game.prototype.getDirection = function (x, y) {
         if (Math.abs(x) > Math.abs(y)) {
--- Game2048/src/2048.ts
+++ Game2048/src/2048.ts
@@ -6,17 +6,10 @@ class Game extends egret.DisplayObjectContainer {
     private m_isMerge:boolean = false;
     private m_isOver:boolean = false;
     private m_isMoved:boolean = false;
-    private m_aniDataArr:Array<Data>;
-    private m_aniCount:number = 0;
 
     public constructor(){
         super();
-        this.m_aniDataArr = [];
 
-        // for (var i = 0; i < Main.m_sNumX * Main.m_sNumY; i++ )
-        // {
-        //     this.m_isGrid[i] = false;
-        // }
         this.addEventListener(egret.Event.ADDED_TO_STAGE,this.onAddToStage,this);
     }
 
@@ -30,7 +23,6 @@ class Game extends egret.DisplayObjectContainer {
     {
         for (var i = 0; i < Main.m_sNumX * Main.m_sNumY; i++ )
         {
-            // this.m_isGrid[i] = false;
             if ((undefined !== this.m_grids[i]) && (undefined !== this.m_grids[i].parent))
             {
                 this.m_grids[i].parent.removeChild(this.m_grids[i]);
@@ -171,16 +163,11 @@ class Game extends egret.DisplayObjectContainer {
         let toY = (vCol + 1) * Main.m_sSpace + vCol * Main.m_sGridHeight;
 
         let data:Data = new Data(next, start, toX - fromX, toY - fromY, 50 * time, isMerge);
-        this.m_aniDataArr.push(data);
     }
 
     private merge(startIndexs:number[], nextNum:number, layerNum:number):void
     {
-        if (0 != this.m_aniDataArr.length)
-        {
-            console.log("aniDataArr is not clear, lenth is " + this.m_aniDataArr.length);
-            this.m_aniDataArr.length = 0;
-        }
+
         if (true == this.m_isMerge)
         {
             console.log("now is merging.");
@@ -193,27 +180,21 @@ class Game extends egret.DisplayObjectContainer {
         }
         console.log("----------------------start----------------------");
         this.m_isMerge = true;
+        const mergePromises = [];
         for (let startIndex of startIndexs)
         {
             var next = startIndex + nextNum;
             let end = startIndex + nextNum * (layerNum - 1)
             for (var start = startIndex; start !== end; )
             {
-                if (0 != this.m_aniCount)
-                {
-                    continue;
-                }
-                // while (false == this.m_isGrid[next])
                 while (undefined == this.m_grids[next])
                 {
-                    // if ((next == end) && (false == this.m_isGrid[end]))
                     if ((next == end) && (undefined == this.m_grids[end]))
                     {
                         break;
                     }
                     next = next + nextNum;
                 }
-                // if (false == this.m_isGrid[next])
                 if (undefined == this.m_grids[next])
                 {
                     start = start + nextNum;
@@ -222,67 +203,38 @@ class Game extends egret.DisplayObjectContainer {
                 }
 
                 let lSpace = Math.abs((start - next) / nextNum);
+                        let vRow = start % Main.m_sNumX;
+        let vCol = Math.floor(start / Main.m_sNumY);
+        let fromX = this.m_grids[next].getX();
+        let fromY = this.m_grids[next].getY();
+        let toX = (vRow + 1) * Main.m_sSpace + vRow * Main.m_sGridWidth;
+        let toY = (vCol + 1) * Main.m_sSpace + vCol * Main.m_sGridHeight;
+
                 //移动到空位置
-                // if (false == this.m_isGrid[start])
                 if (undefined == this.m_grids[start])
                 {
-                    this.m_grids[start] = new Grid();
-                    this.m_grids[start].copy(this.m_grids[next]);
-                    this.setPosition(start, this.m_grids[start]);
-                    this.addChild(this.m_grids[start]);
-                    this.m_grids[start].visible = false;
-                    // this.m_isGrid[start] = true;
-                    // this.m_isGrid[next] = false;
+                    this.m_grids[start] = this.m_grids[next];
+                    mergePromises.push(this.moveBy(toX - fromX, toY - fromY, 50 * lSpace, this.m_grids[next]));             
                     this.m_grids[next] = undefined;
-                    this.animateMove(start, next, lSpace, false);
-                    // while(0 < this.m_aniDataArr.length)
-                    // {
-                    //     this.moveAni(this.m_aniDataArr[0], false);
-                    //     this.m_aniDataArr.shift();
-                    // }
-                    // if (undefined !== this.m_grids[next].parent)
-                    // {
-                    //     this.m_grids[next].parent.addChild(this.m_grids[start]);
-                    //     // this.m_grids[next].parent.removeChild(this.m_grids[next]);
-                    //     this.m_grids[next] = undefined;
-                        // this.m_isGrid[start] = true;
-                        // this.m_isGrid[next] = false;
-                    // }
-                    // this.m_isMoved = true;   
                 }
                 //合并
                 else if (this.m_grids[start].getText() == this.m_grids[next].getText())
                 {
-                    // var vGrid:Grid = new Grid();
-                    // let num = Number(this.m_grids[start].getText());
-                    // let txt = String(num * 2);
-                    // let numInfo = Util.getNumInfo(num * 2);
-                    // this.m_grids[start].setText(txt);
-                    // this.m_grids[start].setColor(numInfo.backgroundColor);
+                    let num = Number(this.m_grids[start].getText());
+                    let txt = String(num * 2);
+                    let numInfo = Util.getNumInfo(num * 2);
+                    this.m_grids[start].setText(txt);
+                    this.m_grids[start].setColor(numInfo.backgroundColor);
+                    this.removeChild(this.m_grids[start]);
+                    this.addChild(this.m_grids[start]);
+                    mergePromises.push(this.moveByAndFadeOut(toX - fromX, toY - fromY, 50 * lSpace, this.m_grids[next]));
                     this.m_grids[next] = undefined;
-                    this.animateMove(start, next, lSpace, true);
-                    // while(0 < this.m_aniDataArr.length)
-                    // {
-                    //     this.mergeAni(this.m_aniDataArr[0], false);
-                    //     this.m_aniDataArr.shift();
-                    // }
-                    // vGrid.copy(this.m_grids[start]);
-                    // if ((undefined !== this.m_grids[next].parent) && (undefined !== this.m_grids[start].parent))
-                    // {
-                    //     //巨坑 不重新添加无法更新自己状态
-                    //     // this.m_grids[start].parent.removeChild(this.m_grids[start]);
-                    //     // this.m_grids[start] = vGrid;
-                    //     this.m_grids[next].parent.addChild(this.m_grids[start]);
-                    //     this.m_grids[next].parent.removeChild(this.m_grids[next]);
-                    //     this.m_grids[next] = undefined;
-                    // }
-                    // this.m_isGrid[next] = false;
+
                     start = start + nextNum;
                     next = start + nextNum;
                     continue;
                 }
 
-                // if ((end == next) || ((next == start + nextNum) && (true == this.m_isGrid[next])))
                 if ((end == next) || ((next == start + nextNum) && (undefined != this.m_grids[next])))
                 {
                     start = start + nextNum;
@@ -295,129 +247,49 @@ class Game extends egret.DisplayObjectContainer {
                 }    
             }
         }
-        if (undefined == this.m_aniDataArr)
-        {
-            this.m_isMerge = false;
-        }
-        else
-        {
-            this.m_aniCount = this.m_aniDataArr.length;
-        }
-        //this.animate();
-    }
-
-    private animate():void
-    {
-        while((undefined != this.m_aniDataArr) && (0 < this.m_aniDataArr.length))
-        {
-            console.log("length:" + length);
-            for(var i = 0; i < this.m_aniDataArr.length; i++)
-            {
-                console.log("m_orgGirdNum:" + this.m_aniDataArr[i].m_orgGirdNum);
-                console.log("m_nowGirdNum:" + this.m_aniDataArr[i].m_nowGirdNum);
-                console.log("m_x:" + this.m_aniDataArr[i].m_x);
-                console.log("m_y:" + this.m_aniDataArr[i].m_y);
-                console.log("m_time:" + this.m_aniDataArr[i].m_time);
-                console.log("m_isMerge:" + this.m_aniDataArr[i].m_isMerge);
-                console.log("-------------------------------------------------------");
-            }
-
-            let dataTemp:Data = this.m_aniDataArr[0];
-            if (true == dataTemp.m_isMerge)
-            {
-                this.mergeAni(dataTemp, true);
-            }
-            else
-            {               
-                this.moveAni(dataTemp, true);
-            }
-            this.m_aniDataArr.shift();
-
-        }
-    }
-
-    private mergeAni(dataTemp:Data, out:boolean):void
-    {
-        egret.Tween.get(this.m_grids[dataTemp.m_orgGirdNum])
-            .to({x:dataTemp.m_x, y:dataTemp.m_y}, dataTemp.m_time, egret.Ease.sineIn)
-            .call(() => 
-            {
-                if (undefined !== this.m_grids[dataTemp.m_orgGirdNum])
-                {
-                    let num = Number(this.m_grids[dataTemp.m_nowGirdNum].getText());
-                    let txt = String(num * 2);
-                    let numInfo = Util.getNumInfo(num * 2);
-                    this.m_grids[dataTemp.m_nowGirdNum].setText(txt);
-                    this.m_grids[dataTemp.m_nowGirdNum].setColor(numInfo.backgroundColor);
-                    this.m_grids[dataTemp.m_orgGirdNum].parent.removeChild(this.m_grids[dataTemp.m_nowGirdNum]);
-                    this.m_grids[dataTemp.m_orgGirdNum].parent.addChild(this.m_grids[dataTemp.m_nowGirdNum]);
-                    this.m_grids[dataTemp.m_orgGirdNum].parent.removeChild(this.m_grids[dataTemp.m_orgGirdNum]);
-                    this.m_grids[dataTemp.m_orgGirdNum] = undefined;
-                } 
-                // this.m_isGrid[dataTemp.m_orgGirdNum] = false;
-                this.m_isMoved = true;
-                if (true == out)
-                {
-                    this.judge();
-                }
-            });
-    }
-
-    private moveAni(dataTemp:Data, out:boolean):void
-    {
-        egret.Tween.get(this.m_grids[dataTemp.m_orgGirdNum])
-            .to({x:dataTemp.m_x, y:dataTemp.m_y}, dataTemp.m_time, egret.Ease.sineIn)
-            .call(() => 
-            {
-                this.m_grids[dataTemp.m_nowGirdNum].visible = true;
-                if (undefined !== this.m_grids[dataTemp.m_orgGirdNum])
-                {
-                    this.m_grids[dataTemp.m_orgGirdNum].parent.removeChild(this.m_grids[dataTemp.m_orgGirdNum]);
-                    this.m_grids[dataTemp.m_orgGirdNum] = undefined;
-                    // this.m_isGrid[dataTemp.m_nowGirdNum] = true;
-                    // this.m_isGrid[dataTemp.m_orgGirdNum] = false;
-                } 
-                this.m_isMoved = true; 
-                if (true == out)
-                {
-                    this.judge();
-                }
-            });
-    }
-
-    // public moveByAndFadeOut(x: number, y: number, time: number, grid:Grid) {
-    //     return new Promise(resolve => {
-    //         egret.Tween.get(grid)
-    //             .to({
-    //                 x: x,
-    //                 y: y
-    //             }, time, egret.Ease.sineInOut)
-    //             .to({
-    //                 alpha: 0
-    //             }, 250)
-    //             .call(() => {
-    //                 grid.parent.removeChild(grid);
-    //                 resolve();
-    //             });
-    //     });
-    // }
-
-    private judge():void
-    {
-        this.m_aniCount--;
-        if (0 != this.m_aniCount)
-        {
-            return;
-        }
-        if (true == this.m_isMoved && 0 == this.m_aniDataArr.length)
-        {
-            this.m_isMerge = false;
-            this.randomGrid();
+        if (mergePromises.length > 0) {
+            // 待所有动画结束后， 将 this.m_isMerge 置为 false
+            Promise.all(mergePromises).then(() => {
+                this.m_isMerge = false;
+                this.randomGrid();
             if (true == this.isWin())
             {
                 console.log("YOU GOT IT!");
             }
+            });
         }
+        else {
+            this.m_isMerge = false;
+        }
+        //this.animate();
+    }
+
+    public moveBy(x: number, y: number, time: number, grid:Grid) {
+        return new Promise(resolve => {
+            egret.Tween.get(grid)
+                .to({
+                    x: x,
+                    y: y
+                }, time, egret.Ease.sineInOut)
+                .call(resolve);
+        });
+    }
+
+    public moveByAndFadeOut(x: number, y: number, time: number, grid:Grid) {
+        return new Promise(resolve => {
+            egret.Tween.get(grid)
+                .to({
+                    x: x,
+                    y: y
+                }, time, egret.Ease.sineInOut)
+                .to({
+                    alpha: 0
+                }, 250)
+                .call(() => {
+                    grid.parent.removeChild(grid);
+                    resolve();
+                });
+        });
     }
 
     private getDirection(x:number, y:number):boolean
